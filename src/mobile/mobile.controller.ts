72d327cdf33df22f4d7ff3680724559bd3c16e0a
KritKhao <kritkhao@KritKhaos-MacBook-Air.local>
2024-09-28 09:58:47 +0700

    split.pipe

--- src/mobile/mobile.controller.ts
+++ src/mobile/mobile.controller.ts
@@ -1,15 +1,27 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CardService } from './card.service';
+import { MobileService } from './mobile.service';
+import { CommaSplitPipe } from './comma-split.pipe';
 
 @Controller('mobile')
 export class MobileController {
-  constructor(private cardService: CardService) {}
+  constructor(
+    private cardService: CardService,
+    private mobileService: MobileService,
+  ) {}
 
   @Get('/name-card/:name/:mobileNo')
   cardName(
     @Param('name') name: string,
     @Param('mobileNo') mobileNo: string,
   ): string {
-    return this, this.cardService.nameCard(name, mobileNo);
+    return this.cardService.nameCard(name, mobileNo);
+  }
+
+  @Get('/blocked-mobile-nos/:mobileNos')
+  blockedMobileNos(
+    @Param('mobileNos', CommaSplitPipe) mobileNos: string[],
+  ): string {
+    return this.mobileService.blockedMobileNos(mobileNos);
   }
 }
